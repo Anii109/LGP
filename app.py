c843f2d728000d32c84a291cde7d15a80d091af8
Aniruddh <aniruddhakhasanis@gmail.com>
2025-06-14 22:51:36 +0530

    Update in app.py for generating the pkl files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 import pickle
 import pandas as pd
 import requests
@@ -70,3 +70,106 @@ if st.button("Recommend"):
         with col:
             st.text(names[idx])
             st.image(posters[idx])
+'''
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import os
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem.porter import PorterStemmer
+
+# Load data or regenerate
+@st.cache_data
+def load_and_prepare_data():
+    movies = pd.read_csv('tmdb_5000_movies.csv')
+    credits = pd.read_csv('tmdb_5000_credits.csv')
+    movies = movies.merge(credits, on='title')
+    movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+    movies.dropna(inplace=True)
+
+    def convert(obj):
+        L = []
+        for i in ast.literal_eval(obj):
+            L.append(i['name'])
+        return L
+
+    def convert3(obj):
+        L = []
+        counter = 0
+        for i in ast.literal_eval(obj):
+            if counter != 3:
+                L.append(i['name'])
+                counter += 1
+            else:
+                break
+        return L
+
+    def fetch_director(obj):
+        for i in ast.literal_eval(obj):
+            if i['job'] == 'Director':
+                return [i['name']]
+        return []
+
+    movies['genres'] = movies['genres'].apply(convert)
+    movies['keywords'] = movies['keywords'].apply(convert)
+    movies['cast'] = movies['cast'].apply(convert3)
+    movies['crew'] = movies['crew'].apply(fetch_director)
+    movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+    for feature in ['genres', 'keywords', 'cast', 'crew']:
+        movies[feature] = movies[feature].apply(lambda x: [i.replace(" ", "") for i in x])
+
+    movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+    new_df = movies[['movie_id', 'title', 'tags']]
+
+    new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x).lower())
+
+    ps = PorterStemmer()
+    def stem(text):
+        return " ".join([ps.stem(i) for i in text.split()])
+
+    new_df['tags'] = new_df['tags'].apply(stem)
+
+    cv = CountVectorizer(max_features=5000, stop_words='english')
+    vectors = cv.fit_transform(new_df['tags']).toarray()
+    similarity = cosine_similarity(vectors)
+
+    # Save for reuse
+    pickle.dump(new_df, open('movies.pkl', 'wb'))
+    pickle.dump(similarity, open('similarity.pkl', 'wb'))
+
+    return new_df, similarity
+
+# Load or generate
+if not os.path.exists('movies.pkl') or not os.path.exists('similarity.pkl'):
+    movies_df, similarity = load_and_prepare_data()
+else:
+    movies_df = pickle.load(open('movies.pkl', 'rb'))
+    similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(movie):
+    movie = movie.lower()
+    try:
+        movie_index = movies_df[movies_df['title'].str.lower() == movie].index[0]
+    except IndexError:
+        return []
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    return [movies_df.iloc[i[0]].title for i in movies_list]
+
+# Streamlit UI
+st.title('🎬 Movie Recommender System')
+
+selected_movie = st.selectbox("Type or select a movie:", movies_df['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie)
+    if recommendations:
+        st.subheader("Top 5 Recommendations:")
+        for i, movie in enumerate(recommendations, 1):
+            st.write(f"{i}. {movie}")
+    else:
+        st.error("Movie not found or insufficient data.")
